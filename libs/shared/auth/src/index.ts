14d5f85424b67c8a2f24d59cb83c368415f73279
Aman Pratap Singh <amanps@zohomail.in>
2025-12-22 18:37:28 +0530

    adding role and role guard

--- libs/shared/auth/src/index.ts
+++ libs/shared/auth/src/index.ts
@@ -1,4 +1,7 @@
+export * from './lib/enums/role.enum';
 export * from './lib/shared-auth.module';
+export * from './lib/guards/roles.guard';
 export * from './lib/guards/jwt-auth.guard';
 export * from './lib/strategies/jwt.strategy';
 export * from './lib/decorators/public.decorator';
+export * from './lib/decorators/roles.decorator';
---
+++ libs/shared/auth/src/lib/guards/roles.guard.ts
@@ -0,0 +1,35 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+    if (!user) {
+      return false;
+    }
+
+    const hasRole = requiredRoles.some((role) => user.roles?.includes(role));
+    if (!hasRole) {
+      throw new ForbiddenException('Insufficient Permissions');
+    }
+
+    return true;
+  }
+}
