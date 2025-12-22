3e80a8a5fe2aa633e3c55947d81b626ae5b26899
Aman Pratap Singh <amanps@zohomail.in>
2025-12-22 17:59:08 +0530

    adding rest api proxy, rate limiting, distributed tracing, redis cache

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,11 +1,52 @@
-import { Module } from '@nestjs/common';
+import Keyv from 'keyv';
+import KeyvRedis from '@keyv/redis';
+import proxy from '@fastify/http-proxy';
+import { FastifyInstance } from 'fastify';
+import { CacheModule } from '@nestjs/cache-manager';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino/LoggerModule';
+import { APP_GUARD, HttpAdapterHost } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+
+import { JwtAuthGuard, SharedAuthModule } from '@clinix/shared/auth';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const redisHost = configService.get('REDIS_HOST') || 'localhost';
+        const rediPort = configService.get('REDIS_PORT') || '6379';
+        const redisUrl = `redis://${redisHost}:${rediPort}`;
+
+        return {
+          stores: [
+            new Keyv({
+              store: new KeyvRedis({
+                url: redisUrl,
+              }),
+            }),
+          ],
+          ttl: 5000,
+        };
+      },
+      inject: [ConfigService],
+    }),
     LoggerModule.forRoot({
       pinoHttp: {
         transport:
@@ -17,11 +58,38 @@ import { AppService } from './app.service';
                 },
               }
             : undefined,
+        genReqId: (req) => req.headers['x-request-id'] || crypto.randomUUID(),
         redact: { paths: ['req.headers.authorization', 'req.body.password'] },
       },
     }),
+    SharedAuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  async onModuleInit() {
+    const app = this.httpAdapterHost.httpAdapter.getInstance<FastifyInstance>();
+    await app.register(async (subApp) => {
+      subApp.removeContentTypeParser('application/json');
+      await subApp.register(proxy, {
+        upstream: 'http://localhost:5001',
+        prefix: '/auth',
+        rewritePrefix: '/api/v1/auth',
+        http2: false,
+      });
+    });
+  }
+}
